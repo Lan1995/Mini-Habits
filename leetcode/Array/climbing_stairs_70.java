120f48720a8ad2da2ace8378158c5117d552f866
= <=>
2020-12-31 23:38:08 +0800

    array

--- leetcode/Array/climbing_stairs_70.java
+++ leetcode/Array/climbing_stairs_70.java
@@ -67,4 +67,8 @@ public class climbing_stairs_70 {
     }
     return r;
   }
+
+  public static void main(String[] args) {
+    System.out.print("hello vs");
+  }
 }
---
+++ leetcode/Array/two_num_1.java
@@ -0,0 +1,37 @@
+package Array;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ *
+ #### 思路漏洞
+ 1. 没考虑到数组中重复的情况，
+ 2. 用了两次for循环
+ 3. 由于两次循环，出现重复情况之后再思考hashmap应该保留哪个索引
+ */
+public class two_num_1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
