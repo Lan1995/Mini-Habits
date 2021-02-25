b26c78372d5a70666ba5b24ae2450cf5e46739a4
qing.lan <qing.lan@leyantech.com>
2021-02-25 23:08:03 +0800

    bfs and dfs

--- leetcode/Tree/BFS/word_ladder_127.java
+++ leetcode/Tree/BFS/word_ladder_127.java
@@ -1,7 +1,49 @@
 package Tree.BFS;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class word_ladder_127 {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-
+        int step = 0;
+        if(beginWord == null || endWord == null || beginWord.length() != endWord.length()) return step;
+        Set<String> wordSet = new HashSet<>(wordList);
+        Deque<String> queue = new ArrayDeque<>();
+        Set<String> visited = new HashSet<>();
+        queue.offer(beginWord);
+        List<Character> changeChar = new ArrayList<>();
+        for (Character character : beginWord.toCharArray()) {
+            changeChar.add(character);
+        }
+        wordList.forEach(e -> {
+            for (Character character : e.toCharArray()) {
+                changeChar.add(character);
+            } 
+        });
+        while(!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                String node = queue.poll();
+                System.out.println("step:" + step +",node:" + node);
+                if(node.equals(endWord)) return step;           
+                for (int j = 0; j < node.toCharArray().length; j++) {
+                    for (int k = 0; k < changeChar.size(); k++) {
+                        char[] old = node.toCharArray();
+                        old[j] = changeChar.get(k);
+                        String newNode = new String(old);
+                        if(wordSet.contains(newNode) && !visited.contains(newNode)) {
+                            queue.offer(newNode);
+                            visited.add(newNode);
+                        }     
+                    }
+                }  
+                step++;            
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        word_ladder_127 test = new word_ladder_127();
+        System.out.println(test.ladderLength("hit", "cog", Arrays.asList("hot","dot","dog","lot","log","cog")));
     }
 }
